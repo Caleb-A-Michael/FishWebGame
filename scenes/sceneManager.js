788555292731329b58651bd5d3a7c3e96ecf8ce4
Caleb-A-Michael <michae93@purdue.edu>
2026-04-07 21:02:27 -0400

    Fix sceneManager onEnter call

--- scenes/sceneManager.js
+++ scenes/sceneManager.js
@@ -2,7 +2,7 @@ let currentScene = null;
 
 export function setScene(scene) {
     currentScene = scene;
-    if (currentScene.onEnter()) currentScene.onEnter();
+    if (currentScene) currentScene.onEnter();
 }
 
 export function update(deltaTime) {
